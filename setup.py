f5869f36bfbf187142a7dd94a699c30f14faec06
Priyanshu <priyanshu24003@gmail.com>
2026-05-01 17:42:27 +0530

    added src_utils as python package

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Priyanshu",
+    author_email="priyan*******@gmail.com",
+    packages=find_packages()
+)
