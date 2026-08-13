5f6a4d17d9cf26e1927eafd0fe4cb82d988caefd
Priyanshu <priyanshu24003@gmail.com>
2026-08-13 06:51:18 +0530

    brought the model retraining component

--- app_mic2/app.py
+++ app_mic2/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import requests
 import time
 from src_utils import Credentials, connect_s3
 import pandas as pd
@@ -33,19 +34,47 @@ def prediction():
         label_maps = dict(zip(range(len(encoder.classes_)), encoder.classes_))      
 
         # Making prediction using Production model.
-        prediction = "pass" if label_maps[model.predict(input_data)[0]]=="Yes" else "Fail"
+        prediction = "PASS" if label_maps[model.predict(input_data)[0]]=="Yes" else "FAIL"
         message = f"This Student is more likely to  {prediction} the exam."
 
     return render_template('index.html', message=message)    
 
-        
+
 @app.route("/train", methods=["GET", "POST"])
 def retrain_model():
 
     #This Url intracts with 3 different microservices (Ingestion, trainer, pusher)
-    
 
-    return render_template("index.html", train_status="Training is Complete")
+    #local
+    # Urls = {
+    #     "Ingest":"http://localhost:3333/train",
+    #     "Trainer":"http://localhost:4444/train",
+    #     "Pusher":"http://localhost:5555/train",
+    # }
+
+    #Production
+    Urls = {
+        "Ingest": Credentials.INGEST_SERVICE_URL,
+        "Trainer": Credentials.TRAINER_SERVICE_URL,
+        "Pusher": Credentials.PUSHER_SERVICE_URL,
+    }
+
+    TrainStatus = True
+    #Running Entire Pipeline.
+    for key, value in Urls.items():
+        print(f"Running Stage :{key}")
+        try:
+            response = requests.get(value, timeout=5)
+            print(response.raise_for_status)
+            print(response.text)
+            print(f"Stage {key} Finish")
+            continue
+        except Exception as e:
+            print(f"Error in {key} stage : {e}")
+            TrainStatus = False
+            break
+
+    return render_template("index.html", train_status="Training is Complete" if TrainStatus == True else "Training Wasn't Successfull" )
 
 
 
