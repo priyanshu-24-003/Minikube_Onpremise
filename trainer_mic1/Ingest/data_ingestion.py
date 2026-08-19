e9afae6b2e922b105c43bf2c3e554f119af156f1
Priyanshu <priyanshu24003@gmail.com>
2026-08-19 12:25:52 +0530

    loggin , ingest

--- trainer_mic1/Ingest/data_ingestion.py
+++ trainer_mic1/Ingest/data_ingestion.py
@@ -2,7 +2,7 @@ from pandas import DataFrame, read_csv
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from src_utils import Credentials, connect_s3, save_data, remove_data, save_model_typ
-
+import logging
 
 
 class Ingestion():
@@ -18,46 +18,65 @@ class Ingestion():
         # self.df = read_csv(self.url) 
 
         #production
-        self.client = connect_s3(Credentials.s3_bucket, Credentials.aws_access_key, Credentials.aws_secret_key)
-        self.df = self.client.fetch_df(self.url)
-    
-    def Encoding(self,)->LabelEncoder:
-        Le = LabelEncoder()
-        Le.fit(self.df['passed'])
-        self.df['passed'] = Le.transform(self.df['passed'])
+        try:
+            self.client = connect_s3(Credentials.s3_bucket, Credentials.aws_access_key, Credentials.aws_secret_key)
+            self.df = self.client.fetch_df(self.url)
+            logging.info(f"connected to S3 inside {__name__}")
 
-        return Le
+        except Exception as e:
+            logging.error(f"Error {e} in {__name__} while connecting to S3 inside Ingest Method")
+        
+    def Encoding(self,)->LabelEncoder:
+        try:
+            Le = LabelEncoder()
+            Le.fit(self.df['passed'])
+            self.df['passed'] = Le.transform(self.df['passed'])
+            logging.info(f"Method Encoding Succesfull inside {__name__}")
+            return Le
+        except Exception as e:
+            logging.error(f"Error {e} in {__name__} inside Encoding Method")
+
+
         
     def Spliting(self)->tuple:
-        train, test = train_test_split(self.df, test_size=0.2, )
-
-        return (train, test)
+        try:    
+            train, test = train_test_split(self.df, test_size=0.2, )
+            logging.info(f"Method Spliting Succesfull inside {__name__}")
+            return (train, test)
+        except Exception as e:
+            logging.error(f"Error {e} in {__name__} inside Spliting Method")
 
 
     def Ingesting(self):
-        self.Ingest()
-        encoder = self.Encoding()
-        train,test = self.Spliting()
-        print(train.head(4), train.shape)
-        print()
-        print(test.head(4), test.shape)
+        try:
+            logging.info("Data Ingestion Started...")
+            self.Ingest()
+            encoder = self.Encoding()
+            train,test = self.Spliting()
+            print(train.head(4), train.shape)
+            print()
+            print(test.head(4), test.shape)
 
-        train_path = "./data/bin/train_ingest.csv"
-        test_path = "./data/bin/test_ingest.csv"
-        encoder_path = "./data/bin/encoder.pkl"
+            train_path = "./data/bin/train_ingest.csv"
+            test_path = "./data/bin/test_ingest.csv"
+            encoder_path = "./data/bin/encoder.pkl"
 
-        save_data(data=train, path=train_path)
-        save_data(data=test, path=test_path)
-        save_model_typ(encoder, encoder_path)
+            save_data(data=train, path=train_path)
+            save_data(data=test, path=test_path)
+            save_model_typ(encoder, encoder_path)
 
-        self.client.push_data(train_path, f'ingest/train.csv')
-        self.client.push_data(test_path, f'ingest/test.csv')
-        self.client.push_data(encoder_path, f"model/encoder")
-        
-        remove_data(path=train_path)
-        remove_data(path=test_path)
-        remove_data(path=encoder_path)
-
+            self.client.push_data(train_path, f'ingest/train.csv')
+            self.client.push_data(test_path, f'ingest/test.csv')
+            self.client.push_data(encoder_path, f"model/encoder")
+            
+            remove_data(path=train_path)
+            remove_data(path=test_path)
+            remove_data(path=encoder_path)
+            logging.info('Data Ingestion Completed')
+            return True
+        except Exception as e:
+            logging.error(f"Error {e} in {__name__} inside Ingesting Method")
+            return False
         
 
 if __name__ == "__main__":
